6b2e2584bf888406bd8961325f5e58d913397c58
kihyeon <psionic4679@gmail.com>
2023-07-05 15:28:55 +0900

    add prime factor test tdd

--- prime-factors_test/test.cpp
+++ prime-factors_test/test.cpp
@@ -1,10 +1,52 @@
 #include "pch.h"
 #include "../prime-factors/prime-factors.cpp"
 
-TEST(PrimeFactors, PrimeTest) {
-  EXPECT_EQ(1, 1);
-  EXPECT_EQ(1, 1);
-  EXPECT_EQ(1, 1);
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class PrimeFixture :public testing::Test
+{
+public:
+	PrimeFactors pf;
+	vector<int> expected;
+};
+
+TEST_F(PrimeFixture, PrimeTest) {
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
+
+TEST_F(PrimeFixture, PrimeOfTest1) {
+	expected = {};
+	EXPECT_EQ(expected, pf.of(1));
+}
+
+TEST_F(PrimeFixture, PrimeOfTest2) {
+	expected = { 2 };
+	EXPECT_EQ(expected, pf.of(2));
+}
+
+TEST_F(PrimeFixture, PrimeOfTest3) {
+	expected = { 3 };
+	EXPECT_EQ(expected, pf.of(3));
+}
+
+TEST_F(PrimeFixture, PrimeOfTest4) {
+	expected = {2,2 };
+	EXPECT_EQ(expected, pf.of(4));
+}
+
+TEST_F(PrimeFixture, PrimeOfTest6) {
+	expected = { 2,3 };
+	EXPECT_EQ(expected, pf.of(6));
+}
+
+TEST_F(PrimeFixture, PrimeOfTest9) {
+	expected = { 3,3 };
+	EXPECT_EQ(expected, pf.of(9));
+}
+
+TEST_F(PrimeFixture, PrimeOfTest12) {
+	expected = { 2,2,3 };
+	EXPECT_EQ(expected, pf.of(12));
 }
